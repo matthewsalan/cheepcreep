42807ec54606236016746e83bce5a5b5ea7b0ef3
Alan Matthews <matthewsa1320@gmail.com>
2015-01-26 19:56:38 -0500

    Added get_followers and get_user functions

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -3,24 +3,44 @@ require "cheepcreep/init_db"
 require "httparty"
 require "pry"
 
+
+
 module Cheepcreep
+	class GitHubUser <ActiveRecord::Base
+	end
 end
 
 class Github
   include HTTParty
   base_uri 'https://api.github.com'
+   
+  def initialize(user = "apitestfun", pass = "ironyard1")
+    @auth = {:username => user, :password => pass}
+  end
 
-  def initialize
-    # ENV["FOO"] is like echo $FOO
-    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+  def get_followers(input = "redline6561", options = {})
+  	options.merge!({:basic_auth => @auth})
+  	resp = self.class.get("/users/#{input}/followers", options)
+  	JSON.parse(resp.body)
+  end
+
+  def get_user(input = "matthewsalan", options = {})
+  	options.merge!({:basic_auth => @auth})
+  	resp = self.class.get("/users/#{input}", options)
+  	JSON.parse(resp.body)
   end
 end
 
 
-class CheepcreepApp
-end
+
+github = Github.new
+github.get_user
+#github.get_followers
 
 binding.pry
+#user = Cheepcreep::GitHubUser.create(JSON.parse(resp.body))
 
-creeper = CheepcreepApp.new
-creeper.creep
+#creeper = CheepcreepApp.new
+#reeper.creep
+
+
